3e1f35a6b896237886ed781b40fef092346d1237
Nicholas <69809504+Scryguy101@users.noreply.github.com>
2020-12-04 10:26:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ basic.show_string("RECIEVER")
 radio.set_group(125)
 serial.write_line("Acceleration")
 
-
-
 def on_received_value(name, value):
     led.toggle(4, 4)
     serial.write_value(name, value)
